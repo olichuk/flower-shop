888ff4262cd7055df3faa33141bfd9ea3b62ca53
Olha Harnyk <garnik12122005@gmail.com>
2026-04-22 21:08:39 +0300

    fix: change navbar in CheckoutPage

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,5 +1,19 @@
-// HomePage.jsx
-const CheckoutPage = () => {
-  return <div>Checkout Page</div>;
-};
-export default CheckoutPage;
+import { Rose } from "lucide-react";
+
+import "./CheckoutPage.css";
+
+export default function CheckoutPage() {
+  return (
+    <div className="page">
+      {/* Navbar */}
+      <nav className="navbar">
+        <a href="/" className="navbar__logo">
+          <span className="navbar__logo-icon">
+            <Rose />
+          </span>
+          F.LOVER
+        </a>
+      </nav>
+    </div>
+  );
+}
