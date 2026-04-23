f855db19e6a466c10b271bb83d1371e61afbc032
Olha Harnyk <garnik12122005@gmail.com>
2026-04-23 21:38:12 +0300

    feat: add admin page for order management with status updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductDetails";
 import CheckoutPage from "./pages/CheckoutPage";
+import AdminPage from "./pages/AdminPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/product/:id" element={<ProductPage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/admin" element={<AdminPage />} />
       </Routes>
     </BrowserRouter>
   );
